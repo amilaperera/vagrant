c7b6aa4aa09dbab77d27163edb24d076385807d1
Amila Perera <Amila.Perera@dr.local>
2017-10-20 10:16:48 +1100

    lubuntu install

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -3,6 +3,9 @@ sudo apt-get update -y
 
 sudo apt-get install vim zsh tmux git python -y
 
+# To install a minimum lubuntu-desktop if the particular vagrant doesn't provide one
+# sudo apt-get install lubuntu-desktop -y
+
 [[ ! -d ~/.dotfiles ]] && git clone https://github.com/amilaperera/dotfiles ~/.dotfiles
 
 echo "Provisioning Done..."
