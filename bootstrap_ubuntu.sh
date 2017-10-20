76177293e35f3b2c713453c0802e0cbc78960034
Amila Perera <Amila.Perera@dr.local>
2017-10-20 16:38:10 +1100

    latest vim

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
 
+# vim8 repository
+sudo add-apt-repository ppa:jonathonf/vim -y
+# and and update afterwards
+sudo apt-get update -y
+
+# mandatory installations first
 sudo apt-get install vim zsh tmux git python -y
 
 # To install a minimum lubuntu-desktop if the particular vagrant doesn't provide one
